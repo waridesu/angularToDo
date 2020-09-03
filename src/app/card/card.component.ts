54729ca04da85816e57451cc1e97efacc125a2eb
Denis <waridesu@gmail.com>
2020-09-03 23:20:13 +0300

    card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.componenet.sass']
+})
+
+export class CardComponent {
+    title= 'My Card Tytle'
+}
